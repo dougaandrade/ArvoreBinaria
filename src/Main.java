457a54bf4892030c4c883f0f04f5d89a8d50bc55
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2025-05-06 10:57:41 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,13 @@ public class Main {
         arvore.inserir(new Folha(45));
         arvore.inserir(new Folha(7));
         arvore.inserir(new Folha(19));
+        arvore.inserir(new Folha(13));
+        arvore.inserir(new Folha(41));
+        arvore.inserir(new Folha(97));
+        arvore.inserir(new Folha(49));
         System.out.println("\n __________Amplitude_________ \n");
-        arvore.amplitude(7);
+        arvore.amplitude(19);
+        arvore.amplitude(97);
         System.out.println("\n __________Grau_________ \n");
         arvore.exibirGraus();
 
